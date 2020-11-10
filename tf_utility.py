057fba2965d8f67a6cff4736b418ea03e54658ef
aliprf <aliprf@gmail.com>
2020-11-10 11:38:26 -0700

    map creation revised

--- tf_utility.py
+++ tf_utility.py
@@ -148,7 +148,7 @@ class TfUtility:
                     img_name = file
                     lbl = np.load(os.path.join(annotation_file_paths[index], str(file)[:-3] + "npy"))
 
-                    landmark_key = self._encode_tf_file_name(lbl.tostring().encode('utf-8'))
+                    landmark_key = self._encode_tf_file_name(lbl.tostring())
 
                     # landmark_key = lbl.tostring()
                     map[landmark_key] = img_name
